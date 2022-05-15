aeac42acd3336fef28400a5f57e17125fda0a78c
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-05-15 16:10:55 +0600

    admin route  added for admin

--- index.js
+++ index.js
@@ -44,11 +44,34 @@ async function run() {
       .collection("booking");
     const userCollection = client.db("doctors_portals").collection("users");
     /* -----------User Api Area----------- */
-    app.get('/user', async (req, res) => {
-      const users = await userCollection.find().toArray()
-      res.send(users)
-    })
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updatedData = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updatedData);
+        res.send(result);
+      } else {
+        return res.status(403).send({ message: "Forbidded" });
+      }
+    });
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
